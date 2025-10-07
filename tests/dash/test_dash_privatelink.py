9e9f970361a91e9fcea0dd72207d5893ffe578d1
Lawrence Lee <lawlee@microsoft.com>
2025-10-07 16:03:38 -0700

    [dash] Add tests to cover PLNSG, FNIC, trusted VNI, return path ECMP (#19700)
    Add test to cover DASH features PLNSG, floating NIC, trusted VNI, and return path ECMP

--- tests/dash/test_dash_privatelink.py
+++ tests/dash/test_dash_privatelink.py
@@ -3,64 +3,41 @@ import logging
 import configs.privatelink_config as pl
 import ptf.testutils as testutils
 import pytest
-from constants import LOCAL_PTF_INTF, LOCAL_DUT_INTF, REMOTE_DUT_INTF, REMOTE_PTF_RECV_INTF, REMOTE_PTF_SEND_INTF
+from constants import LOCAL_PTF_INTF, REMOTE_PTF_RECV_INTF, REMOTE_PTF_SEND_INTF
 from gnmi_utils import apply_messages
 from packets import outbound_pl_packets, inbound_pl_packets
-from tests.dash.conftest import get_interface_ip
+from tests.common.config_reload import config_reload
 
 logger = logging.getLogger(__name__)
 
 pytestmark = [
     pytest.mark.topology('t1'),
+    pytest.mark.topology('smartswitch'),
     pytest.mark.skip_check_dut_health
 ]
 
 
 """
 Test prerequisites:
-- DPU needs the Appliance VIP configured as its loopback IP
 - Assign IPs to DPU-NPU dataplane interfaces
-- Default route on DPU to NPU
 """
 
 
-@pytest.fixture(scope="module")
-def use_pkt_alt_attrs(duthost):
-    hwsku = duthost.sonichost._facts["hwsku"]
-    if hwsku == "Cisco-8102-28FH-DPU-O-T1":
-        return True
-    else:
-        return False
-
-
-@pytest.fixture(scope="module", autouse=True)
-def add_npu_static_routes(duthost, dpu_ip, dash_pl_config, skip_config, skip_cleanup):
-    if not skip_config:
-        cmds = []
-        vm_nexthop_ip = get_interface_ip(duthost, dash_pl_config[LOCAL_DUT_INTF]).ip + 1
-        pe_nexthop_ip = get_interface_ip(duthost, dash_pl_config[REMOTE_DUT_INTF]).ip + 1
-        cmds.append(f"ip route replace {pl.APPLIANCE_VIP}/32 via {dpu_ip}")
-        cmds.append(f"ip route replace {pl.VM1_PA}/32 via {vm_nexthop_ip}")
-        cmds.append(f"ip route replace {pl.PE_PA}/32 via {pe_nexthop_ip}")
-        logger.info(f"Adding static routes: {cmds}")
-        duthost.shell_cmds(cmds=cmds)
-
-    yield
-
-    if not skip_config and not skip_cleanup:
-        cmds = []
-        cmds.append(f"ip route del {pl.APPLIANCE_VIP}/32 via {dpu_ip}")
-        cmds.append(f"ip route del {pl.VM1_PA}/32 via {vm_nexthop_ip}")
-        cmds.append(f"ip route del {pl.PE_PA}/32 via {pe_nexthop_ip}")
-        logger.info(f"Removing static routes: {cmds}")
-        duthost.shell_cmds(cmds=cmds)
-
-
-@pytest.fixture(autouse=True)
-def common_setup_teardown(localhost, duthost, ptfhost, dpu_index, skip_config):
+@pytest.fixture(autouse=True, scope="function")
+def common_setup_teardown(
+    localhost,
+    duthost,
+    ptfhost,
+    dpu_index,
+    skip_config,
+    dpuhosts,
+    set_vxlan_udp_sport_range,
+    setup_npu_dpu  # noqa: F811
+):
     if skip_config:
         return
 
+    dpuhost = dpuhosts[dpu_index]
     logger.info(pl.ROUTING_TYPE_PL_CONFIG)
     base_config_messages = {
         **pl.APPLIANCE_CONFIG,
@@ -95,11 +72,13 @@ def common_setup_teardown(localhost, duthost, ptfhost, dpu_index, skip_config):
     apply_messages(localhost, duthost, ptfhost, pl.ENI_ROUTE_GROUP1_CONFIG, dpu_index)
 
     yield
-    apply_messages(localhost, duthost, ptfhost, pl.ENI_ROUTE_GROUP1_CONFIG, dpu_index, False)
-    apply_messages(localhost, duthost, ptfhost, pl.ENI_CONFIG, dpu_index, False)
-    apply_messages(localhost, duthost, ptfhost, meter_rule_messages, dpu_index, False)
-    apply_messages(localhost, duthost, ptfhost, route_and_mapping_messages, dpu_index, False)
-    apply_messages(localhost, duthost, ptfhost, base_config_messages, dpu_index, False)
+
+    config_reload(dpuhost, safe_reload=True, yang_validate=False)
+    # apply_messages(localhost, duthost, ptfhost, pl.ENI_ROUTE_GROUP1_CONFIG, dpuhost.dpu_index, False)
+    # apply_messages(localhost, duthost, ptfhost, pl.ENI_CONFIG, dpuhost.dpu_index, False)
+    # apply_messages(localhost, duthost, ptfhost, meter_rule_messages, dpuhost.dpu_index, False)
+    # apply_messages(localhost, duthost, ptfhost, route_and_mapping_messages, dpuhost.dpu_index, False)
+    # apply_messages(localhost, duthost, ptfhost, base_config_messages, dpuhost.dpu_index, False)
 
 
 @pytest.mark.parametrize("encap_proto", ["vxlan", "gre"])
@@ -108,8 +87,8 @@ def test_privatelink_basic_transform(
     dash_pl_config,
     encap_proto
 ):
-    vm_to_dpu_pkt, exp_dpu_to_pe_pkt = outbound_pl_packets(dash_pl_config, encap_proto, use_pkt_alt_attrs)
-    pe_to_dpu_pkt, exp_dpu_to_vm_pkt = inbound_pl_packets(dash_pl_config, use_pkt_alt_attrs)
+    vm_to_dpu_pkt, exp_dpu_to_pe_pkt = outbound_pl_packets(dash_pl_config, encap_proto)
+    pe_to_dpu_pkt, exp_dpu_to_vm_pkt = inbound_pl_packets(dash_pl_config)
 
     ptfadapter.dataplane.flush()
     testutils.send(ptfadapter, dash_pl_config[LOCAL_PTF_INTF], vm_to_dpu_pkt, 1)
